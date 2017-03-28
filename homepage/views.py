1521408de7ff0e29697b6a117ba166aa332c9602
Meerim <pojiratelnica@gmail.com>
2017-03-28 11:37:45 +0600

    homepage slider

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from models import Slides
 
 # Create your views here.
+
+
+def index(request):
+    slide = Slides.objects.all()
+    count = len(Slides.objects.all())
+    context = {
+        'slides': slide,
+        'count': range(count),
+        'title': 'Hello',
+    }
+    return render(request, 'homepage/index.html', context)
